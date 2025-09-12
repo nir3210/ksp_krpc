ecc55b350fa51670668a371a18182bb699a63eed
nir3210 <n.friedman2010@gmail.com>
2025-09-13 00:14:24 +0300

    auto pilot 0.1.2

--- autopilot_for_f9.py
+++ autopilot_for_f9.py
@@ -5,8 +5,11 @@ import time
 conn = None
 vessel = None
 
+first_stage_cut_off_apo = 73000
+first_stage_pitch_end = 45
+
 def launch_sequence():
-    global vessel
+    global vessel, conn
     control = vessel.control
     flight = vessel.flight(vessel.orbit.body.reference_frame)
     orbit = vessel.orbit
@@ -18,11 +21,31 @@ def launch_sequence():
     vessel.auto_pilot.target_pitch_and_heading(90, 90)
 
     control.activate_next_stage()
+    #launch_time = conn.space_center.ut
     for i in range(101):
         control.throttle = i/100
         time.sleep(0.01)
 
+    time.sleep(0.12)
+
     control.activate_next_stage()
+
+    while True:
+        #ut = conn.space_center
+        #t_since_launch = ut - launch_time
+        apo = orbit.apoapsis_altitude
+        alt = flight.mean_altitude
+        print(f"apo: {apo:.2f}, alt: {alt:.2f}")
+
+        if alt > 500 and apo < first_stage_cut_off_apo:
+            frac = apo / first_stage_cut_off_apo
+            frac = min(frac, 1.0)
+            pitch = 90 - frac * (90 - first_stage_pitch_end)
+            vessel.auto_pilot.target_pitch_and_heading(pitch , 90)
+            print(pitch)
+        time.sleep(0.02)
+
+
 
 
 
@@ -38,7 +61,9 @@ def main():
     vessel.name = "Ghidorah 9 - Crew Rodan"
 
 
-    threading.Thread(target=launch_sequence(), daemon=True).start()
+    threading.Thread(target=launch_sequence, daemon=True).start()
 
 if __name__ == '__main__':
     main()
+    while True:
+        time.sleep(1)
