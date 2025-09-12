d0ddaf1b5a0f5f296f0ec0070c08b3b46f656edb
nir3210 <n.friedman2010@gmail.com>
2025-09-12 22:41:17 +0300

    tel works king off.1

--- test1.py
+++ test1.py
@@ -10,11 +10,16 @@ def get_tel(vessel, flight, orbit):
     vs = flight.vertical_speed
     speed = flight.speed
     altitude = flight.mean_altitude
+    twr = vessel.thrust / (vessel.mass * 9.81) if vessel.mass > 0 else 0
+    mach = flight.mach
+
 
     return {
         "vs":vs,
         "speed":speed,
-        "altitude":altitude
+        "altitude":altitude,
+        "twr":twr,
+        "mach":mach
     }
 
 def main():
@@ -33,7 +38,8 @@ def main():
 
     while True:
         flight_perms = get_tel(vessel, flight, orbit)
-        print(f"VS: {flight_perms['vs']:.2f}m/s, speed: {flight_perms['speed']:.2f}m/s, altitude: {flight_perms['altitude']:.2f}m")
+        print(f"VS: {flight_perms['vs']:.2f}m/s, speed: {flight_perms['speed']:.2f}m/s, altitude: {flight_perms['altitude']:.2f}m, twr: {flight_perms['twr']:.2f}")
+        print(f"mach: {flight_perms['mach']:.2f}")
         time.sleep(0.02)
 
 
